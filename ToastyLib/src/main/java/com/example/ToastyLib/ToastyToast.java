59721e2c6a69d8f73b8a4564b9f6657047ecaf53
Andy Pham <andy.pham@ifar.ca>
2019-07-24 10:16:53 -0400

    random color toast

--- ToastyLib/src/main/java/com/example/ToastyLib/ToastyToast.java
+++ ToastyLib/src/main/java/com/example/ToastyLib/ToastyToast.java
@@ -11,13 +11,13 @@ import java.util.Random;
 
 public class ToastyToast {
 
+
     public static void make(Context mContext, String message){
 
         Toast toast = Toast.makeText(mContext,message, Toast.LENGTH_SHORT);
         //Gets the actual oval background of the Toast then sets the colour filter
         View view = toast.getView();
         TextView text = (TextView) view.findViewById(android.R.id.message);
-
 
         Random rand = new Random();
         int n = rand.nextInt(2);
@@ -28,5 +28,28 @@ public class ToastyToast {
         text.setTextColor(Color.WHITE);
 
         toast.show();
+    }
+
+    public static void makeRandom(Context mContext, String message){
+
+        Toast toast = Toast.makeText(mContext,message, Toast.LENGTH_SHORT);
+        //Gets the actual oval background of the Toast then sets the colour filter
+        View view = toast.getView();
+        TextView text = (TextView) view.findViewById(android.R.id.message);
+
+        view.getBackground().setColorFilter(randomColor(), PorterDuff.Mode.SRC_IN);
+        text.setTextColor(randomColor());
+
+        toast.show();
+    }
+
+    private static int randomColor() {
+
+        Random rand = new Random();
+        int R = rand.nextInt(255);
+        int G = rand.nextInt(255);
+        int B = rand.nextInt(255);
+
+        return Color.rgb(R, G, B);
     }
 }
